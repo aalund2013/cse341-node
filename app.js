ae2e8ce020d7066d7f8d3c456684485c011e3452
aalund2013 <ran08001@byui.edu>
2022-04-23 17:06:01 -0600

    nothing really changed

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const mongodb = require('./db/connection');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app
+app  
+.use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
+  
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,15 +2,16 @@
 const express = require('express');
 var contacts = require('../controllers/contacts');
 var router = express.Router()
+
 module.exports = function(app) {
 
     // route for all contacts
-    // app.route('/contacts')
+    app.route('../controllers/contacts')
     router.get(contacts.get_contacts);
 
     // route for single contact - by id    
-    // app.route('/contacts/:contactId')
-        router.get(contacts.get_contact);
+    app.route('../controllers/contacts/:contactId')
+    router.get(contacts.get_contact);
 };
 
 
