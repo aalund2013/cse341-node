da0a5e187790065f6806f18811a3a3337135af2d
aalund2013 <ran08001@byui.edu>
2022-04-22 23:34:34 -0600

    updated server

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+const professionalRoutes = require('./routes/contacts');
+
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-   res.send('Traci Burgess');
-})
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/professional', professionalRoutes);
 
-app.listen(port, function () {
-  console.log(`Running on port ${port}`)
-})
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
