48163054db37da8b2a4be8c46a61c0388250b85f
aalund2013 <ran08001@byui.edu>
2022-04-29 22:03:09 -0600

    try again

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'Contacts API',
     description: 'A list of my contacts',
   },
-  host: 'https://evening-shelf-20122.herokuapp.com/',
+  host: 'evening-shelf-20122.herokuapp.com/',
   // 'localhost:8080',
   schemes: ['http'],
 };
